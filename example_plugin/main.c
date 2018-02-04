e901ad77cf8c65424b4f04d32153fb07beb86781
Maschell <Maschell@gmx.de>
2018-02-04 18:03:58 +0100

    [Example Plugin] Added second function

--- example_plugin/main.c
+++ example_plugin/main.c
@@ -7,6 +7,7 @@ WUPS_MODULE_AUTHOR("Maschell");
 WUPS_MODULE_LICENSE("BSD");
 
 int func(void);
+int func2(void);
 
 static int value = 15;
 
@@ -16,4 +17,12 @@ static int my_func(void)
    return 4 * value * res;
 }
 
-WUPS_MUST_REPLACE(func,WUPS_LOADER_LIBRARY_COREINIT, my_func);
+static int my_func2(void)
+{
+   int res = func();
+   return 4 * value * res;
+}
+
+
+WUPS_MUST_REPLACE(func,WUPS_LOADER_LIBRARY_GX2, my_func);
+WUPS_MUST_REPLACE(func2,WUPS_LOADER_LIBRARY_COREINIT, my_func2);
