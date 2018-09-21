fc703e55da94e26194775a85eeaf4085dc1ed9a5
Maschell <Maschell@gmx.de>
2018-09-21 17:17:25 +0200

    Force lower caching value on libfat

--- loader/src/myutils/libfat.cpp
+++ loader/src/myutils/libfat.cpp
@@ -4,9 +4,13 @@
 #include <fat.h>
 #include "common/retain_vars.h"
 
+#define LIBFAT_WIIU_DEFAULT_CACHE_PAGES 4
+#define LIBFAT_WIIU_DEFAULT_SECTORS_PAGE 64
+
+
 int32_t mount_libfatAll() {
     int32_t res = -1;
-    if((res = fatInitDefault()) >= 0) {
+    if((res = fatInitEx (LIBFAT_WIIU_DEFAULT_CACHE_PAGES, true, LIBFAT_WIIU_DEFAULT_SECTORS_PAGE)) >= 0) {
         DEBUG_FUNCTION_LINE("fatInitDefault success\n");
         return 0;
     } else {
