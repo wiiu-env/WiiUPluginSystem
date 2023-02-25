54b09fe44c89e825296487cdd4d65feba46fcf84
Maschell <Maschell@gmx.de>
2023-02-25 20:51:48 +0100

    Improve the example documentation and code

---
+++ plugins/example_plugin/Dockerfile
@@ -0,0 +1,5 @@
+FROM wiiuenv/devkitppc:20230218
+
+COPY --from=wiiuenv/wiiupluginsystem:20230215 /artifacts $DEVKITPRO
+
+WORKDIR project
--- plugins/example_plugin/src/main.cpp
+++ plugins/example_plugin/src/main.cpp
@@ -1,13 +1,7 @@
+#include "utils/logger.h"
 #include <coreinit/filesystem.h>
-#include <coreinit/thread.h>
-#include <coreinit/time.h>
 #include <malloc.h>
 #include <string.h>
-#include <utils/logger.h>
-#include <whb/libmanager.h>
-#include <whb/log_cafe.h>
-#include <whb/log_module.h>
-#include <whb/log_udp.h>
 #include <wups.h>
 #include <wups/config/WUPSConfigItemBoolean.h>
 
@@ -21,6 +15,8 @@ WUPS_PLUGIN_VERSION("v1.0");
 WUPS_PLUGIN_AUTHOR("Maschell");
 WUPS_PLUGIN_LICENSE("BSD");
 
+#define LOG_FS_OPEN_CONFIG_ID "logFSOpen"
+
 /**
     All of this defines can be used in ANY file.
     It's possible to split it up into multiple files.
@@ -28,18 +24,16 @@ WUPS_PLUGIN_LICENSE("BSD");
 **/
 
 WUPS_USE_WUT_DEVOPTAB();            // Use the wut devoptabs
-WUPS_USE_STORAGE("example_plugin"); // Unqiue id for the storage api
+WUPS_USE_STORAGE("example_plugin"); // Unique id for the storage api
 
 bool logFSOpen = true;
 
 /**
-    Get's called ONCE when the loader exits, but BEFORE the ON_APPLICATION_START gets called or functions are overridden.
+    Gets called ONCE when the plugin was loaded.
 **/
 INITIALIZE_PLUGIN() {
-    if (!WHBLogModuleInit()) {
-        WHBLogCafeInit();
-        WHBLogUdpInit();
-    }
+    // Logging only works when compiled with `make DEBUG=1`. See the README for more information.
+    initLogging();
     DEBUG_FUNCTION_LINE("INITIALIZE_PLUGIN of example_plugin!");
 
     // Open storage to read values
@@ -48,9 +42,9 @@ INITIALIZE_PLUGIN() {
         DEBUG_FUNCTION_LINE("Failed to open storage %s (%d)", WUPS_GetStorageStatusStr(storageRes), storageRes);
     } else {
         // Try to get value from storage
-        if ((storageRes = WUPS_GetBool(nullptr, "logFSOpen", &logFSOpen)) == WUPS_STORAGE_ERROR_NOT_FOUND) {
+        if ((storageRes = WUPS_GetBool(nullptr, LOG_FS_OPEN_CONFIG_ID, &logFSOpen)) == WUPS_STORAGE_ERROR_NOT_FOUND) {
             // Add the value to the storage if it's missing.
-            if (WUPS_StoreBool(nullptr, "logFSOpen", logFSOpen) != WUPS_STORAGE_ERROR_SUCCESS) {
+            if (WUPS_StoreBool(nullptr, LOG_FS_OPEN_CONFIG_ID, logFSOpen) != WUPS_STORAGE_ERROR_SUCCESS) {
                 DEBUG_FUNCTION_LINE("Failed to store bool");
             }
         } else if (storageRes != WUPS_STORAGE_ERROR_SUCCESS) {
@@ -62,11 +56,11 @@ INITIALIZE_PLUGIN() {
             DEBUG_FUNCTION_LINE("Failed to close storage");
         }
     }
+    deinitLogging();
 }
 
 /**
-    Gets called when the plugin loader is re-entered => when the plugin is unloaded. 
-	The overridden functions are restored before this is getting called.
+    Gets called when the plugin will be unloaded.
 **/
 DEINITIALIZE_PLUGIN() {
     DEBUG_FUNCTION_LINE("DEINITIALIZE_PLUGIN of example_plugin!");
@@ -74,34 +68,39 @@ DEINITIALIZE_PLUGIN() {
 
 /**
     Gets called when an application starts.
-	This is called BEFORE the functions are overridden.
-	Make sure to initialize all functions you're using in the overridden functions!
 **/
 ON_APPLICATION_START() {
-    if (!WHBLogModuleInit()) {
-        WHBLogCafeInit();
-        WHBLogUdpInit();
-    }
+    initLogging();
 
     DEBUG_FUNCTION_LINE("ON_APPLICATION_START of example_plugin!");
+}
+
+/**
+ * Gets called when an application actually ends
+ */
+ON_APPLICATION_ENDS() {
+    deinitLogging();
 }
 
 /**
     Gets called when an application request to exit.
 **/
 ON_APPLICATION_REQUESTS_EXIT() {
-    DEBUG_FUNCTION_LINE("ON_APPLICATION_REQUESTS_EXIT of example_plugin!");
+    DEBUG_FUNCTION_LINE_INFO("ON_APPLICATION_REQUESTS_EXIT of example_plugin!");
 }
 
+/**
+ * Callback that will be called if the config has been changed
+ */
 void logFSOpenChanged(ConfigItemBoolean *item, bool newValue) {
-    DEBUG_FUNCTION_LINE("New value in logFSOpenChanged: %d", newValue);
+    DEBUG_FUNCTION_LINE_INFO("New value in logFSOpenChanged: %d", newValue);
     logFSOpen = newValue;
     // If the value has changed, we store it in the storage.
-    WUPS_StoreInt(nullptr, "logFSOpen", logFSOpen);
+    WUPS_StoreInt(nullptr, LOG_FS_OPEN_CONFIG_ID, logFSOpen);
 }
 
 WUPS_GET_CONFIG() {
-    // We open the storage so we can persist the configuration the user did.
+    // We open the storage, so we can persist the configuration the user did.
     if (WUPS_OpenStorage() != WUPS_STORAGE_ERROR_SUCCESS) {
         DEBUG_FUNCTION_LINE("Failed to open storage");
         return 0;
@@ -113,7 +112,7 @@ WUPS_GET_CONFIG() {
     WUPSConfigCategoryHandle cat;
     WUPSConfig_AddCategoryByNameHandled(config, "Logging", &cat);
 
-    WUPSConfigItemBoolean_AddToCategoryHandled(config, cat, "logFSOpen", "Log FSOpen calls", logFSOpen, &logFSOpenChanged);
+    WUPSConfigItemBoolean_AddToCategoryHandled(config, cat, LOG_FS_OPEN_CONFIG_ID, "Log FSOpen calls", logFSOpen, &logFSOpenChanged);
 
     return config;
 }
@@ -121,14 +120,14 @@ WUPS_GET_CONFIG() {
 WUPS_CONFIG_CLOSED() {
     // Save all changes
     if (WUPS_CloseStorage() != WUPS_STORAGE_ERROR_SUCCESS) {
-        DEBUG_FUNCTION_LINE("Failed to close storage");
+        DEBUG_FUNCTION_LINE_ERR("Failed to close storage");
     }
 }
 
 /**
     This defines a function replacement.
     It allows to replace the system function with an own function. 
-    So whenever a game / application calles an overridden function, your function gets called instead.
+    So whenever a game / application calls an overridden function, your function gets called instead.
     
     Currently it's only possible to override functions that are loaded from .rpl files of OSv10 (00050010-1000400A).
     
@@ -138,7 +137,7 @@ WUPS_CONFIG_CLOSED() {
     }
     
     Within this macro, two more function get declare you can use.
-    my_ARBITRARY_NAME_OF_FUNCTION and real_FSOpenFile
+    my_ARBITRARY_NAME_OF_FUNCTION and real_ARBITRARY_NAME_OF_FUNCTION
     
     RETURN_TYPE my_ARBITRARY_NAME_OF_FUNCTION(ARGS_SEPERATED_BY_COMMA):
         is just name of the function that gets declared in this macro. 
@@ -147,16 +146,14 @@ WUPS_CONFIG_CLOSED() {
     RETURN_TYPE real_ARBITRARY_NAME_OF_FUNCTION(ARGS_SEPERATED_BY_COMMA):
         is the name of the function, that leads to function that was overridden.
         Use this to call the original function that will be overridden.
-        CAUTION: Other plugins may already have manipulated the the return value or arguments.
-        
-    
-    Use this macro for each function you want to override
+        CAUTION: Other plugins may already have manipulated the return value or arguments.
 
+    Use this macro for each function you want to override
 **/
 DECL_FUNCTION(int, FSOpenFile, FSClient *pClient, FSCmdBlock *pCmd, const char *path, const char *mode, int *handle, int error) {
     int result = real_FSOpenFile(pClient, pCmd, path, mode, handle, error);
     if (logFSOpen) {
-        DEBUG_FUNCTION_LINE("FSOpenFile called for folder %s! Result %d", path, result);
+        DEBUG_FUNCTION_LINE_INFO("FSOpenFile called for folder %s! Result %d", path, result);
     }
     return result;
 }
