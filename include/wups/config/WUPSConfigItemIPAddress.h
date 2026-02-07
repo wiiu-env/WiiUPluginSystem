f84549e27a1359a827795c924fd39dd329908536
Maschell <Maschell@gmx.de>
2026-02-07 09:45:56 +0100

    Add WUPSConfigItemIPAddress.h

---
+++ include/wups/config/WUPSConfigItemIPAddress.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <stdint.h>
+#include <wups/config.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum ConfigItemIPAddress_State {
+    WUPS_CONFIG_ITEM_IP_ADDRESS_STATE_NONE,
+    // Make sure they are sequential
+    WUPS_CONFIG_ITEM_IP_ADDRESS_STATE_EDIT_INPUT_FIELD1,
+    WUPS_CONFIG_ITEM_IP_ADDRESS_STATE_EDIT_INPUT_FIELD2,
+    WUPS_CONFIG_ITEM_IP_ADDRESS_STATE_EDIT_INPUT_FIELD3,
+    WUPS_CONFIG_ITEM_IP_ADDRESS_STATE_EDIT_INPUT_FIELD4
+} ConfigItemIPAddress_State;
+
+typedef struct ConfigItemIPAddress {
+    char *identifier;
+    WUPSConfigItemHandle itemHandle;
+    ConfigItemIPAddress_State itemState;
+    uint32_t defaultValue;
+    uint32_t currentValue;
+    uint32_t valueAtCreation;
+    uint32_t valueBeforeEdit;
+    uint32_t frameTimer;
+    void *valueChangedCallback;
+} ConfigItemIPAddress;
+
+typedef void (*IPAddressValueChangedCallback)(ConfigItemIPAddress *item, uint32_t ipAddress);
+
+WUPSConfigAPIStatus
+WUPSConfigItemIPAddress_Create(const char *identifier,
+                               const char *displayName,
+                               uint32_t defaultIPAddress,
+                               IPAddressValueChangedCallback callback,
+                               WUPSConfigItemHandle *outItemHandle);
+
+
+WUPSConfigAPIStatus
+WUPSConfigItemIPAddress_AddToCategory(WUPSConfigCategoryHandle cat,
+                                      const char *identifier, const char *displayName,
+                                      uint32_t defaultIPAddress,
+                                      IPAddressValueChangedCallback callback);
+#ifdef __cplusplus
+}
+#endif
+
+#if defined(__cplusplus) && __cplusplus > 201703L
+
+#include <optional>
+#include <string>
+#include <wups/config/WUPSConfigItem.h>
+
+class WUPSConfigItemIPAddress : public WUPSConfigItem {
+public:
+    static std::optional<WUPSConfigItemIPAddress> Create(const std::optional<std::string> &identifier,
+                                                         std::string_view displayName, uint32_t defaultIPAddress,
+                                                         IPAddressValueChangedCallback callback,
+                                                         WUPSConfigAPIStatus &err) noexcept;
+
+    static WUPSConfigItemIPAddress Create(const std::optional<std::string> &identifier,
+                                          std::string_view displayName,
+                                          uint32_t defaultIPAddress,
+                                          IPAddressValueChangedCallback callback);
+
+private:
+    explicit WUPSConfigItemIPAddress(const WUPSConfigItemHandle itemHandle) : WUPSConfigItem(itemHandle) {
+    }
+};
+#endif
---
+++ libraries/libwups/WUPSConfigItemIPAddressCpp.cpp
@@ -0,0 +1,31 @@
+#include <stdexcept>
+#include <wups/config/WUPSConfigItemIPAddress.h>
+#include <wups/config_api.h>
+
+std::optional<WUPSConfigItemIPAddress> WUPSConfigItemIPAddress::Create(const std::optional<std::string> &identifier,
+                                                                       const std::string_view displayName,
+                                                                       uint32_t defaultValue,
+                                                                       const IPAddressValueChangedCallback callback,
+                                                                       WUPSConfigAPIStatus &err) noexcept {
+    WUPSConfigItemHandle itemHandle;
+    if ((err = WUPSConfigItemIPAddress_Create(identifier ? identifier->data() : nullptr,
+                                              displayName.data(),
+                                              defaultValue,
+                                              callback,
+                                              &itemHandle)) != WUPSCONFIG_API_RESULT_SUCCESS) {
+        return std::nullopt;
+    }
+    return WUPSConfigItemIPAddress(itemHandle);
+}
+
+WUPSConfigItemIPAddress WUPSConfigItemIPAddress::Create(const std::optional<std::string> &identifier,
+                                                        const std::string_view displayName,
+                                                        uint32_t defaultValue,
+                                                        const IPAddressValueChangedCallback callback) {
+    WUPSConfigAPIStatus err = WUPSCONFIG_API_RESULT_UNKNOWN_ERROR;
+    auto res                = Create(identifier, displayName, defaultValue, callback, err);
+    if (!res) {
+        throw std::runtime_error(std::string("Failed to create WUPSConfigItemIPAddress: ").append(WUPSConfigAPI_GetStatusStr(err)));
+    }
+    return std::move(*res);
+}
