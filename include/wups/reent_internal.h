a43b43cba9ac9e7cc5720d93ef27c3892293e023
Maschell <Maschell@gmx.de>
2026-04-14 17:24:15 +0200

    Use non-newlib function to allocate memory for reent structs to make things simpler

--- include/wups/reent_internal.h
+++ include/wups/reent_internal.h
@@ -5,26 +5,16 @@
 extern "C" {
 #endif
 
-typedef enum wups_loader_init_reent_errors_t_ {
-    WUPSReent_ERROR_NONE                   = 0,
-    WUPSReent_ERROR_GLOBAL_REENT_REQUESTED = 1,
-    WUPSReent_ERROR_NO_THREAD              = 2,
-} wups_loader_init_reent_errors_t_;
-
-typedef void *(*WUPSReent_GetReentContext)(const void *pluginId, wups_loader_init_reent_errors_t_ *outError);
-typedef void *(*WUPSReent_SetSentinel)();
-typedef void (*WUPSReent_RestoreHead)(void *oldHead);
-typedef bool (*WUPSReent_AddReentContext)(const void *pluginId, void *reentPtr, void (*cleanupFn)(void *), void *oldHead);
+typedef bool (*WUPSReent_GetReentContext)(const void *pluginId, void **outPtr);
+typedef bool (*WUPSReent_AddReentContext)(const void *pluginId, void *reentPtr, void (*cleanupFn)(void *));
 
 typedef uint32_t WUPS_REENT_API_VERSION;
 
-#define WUPS_REENT_CUR_API_VERSION 0x01
+#define WUPS_REENT_CUR_API_VERSION 0x02
 
 typedef struct wups_loader_init_reent_args_t_ {
     WUPS_REENT_API_VERSION version;
     WUPSReent_GetReentContext get_context_ptr;
-    WUPSReent_SetSentinel set_sentinel_ptr;
-    WUPSReent_RestoreHead restore_head_ptr;
     WUPSReent_AddReentContext add_reent_context_ptr;
 } wups_loader_init_reent_args_t_;
 
